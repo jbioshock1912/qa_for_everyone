09185c27d72b8e135cf71d52005470bf5c188414
jbioshock1912 <loh7457490>
2022-05-26 23:04:18 +0300

    update

--- src/main/java/codewars/fight/Kata.java
+++ src/main/java/codewars/fight/Kata.java
@@ -1,5 +1,13 @@
 package codewars.fight;
 
+/*Codewars Task3
+        Create a function that returns the name of the winner in a fight between two fighters.
+        Each fighter takes turns attacking the other and whoever kills the other first is victorious.
+        Death is defined as having health <= 0.
+        Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+        Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0.
+        You can mutate the Fighter objects.*/
+
 public class Kata {
     private static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
         String winner = "";
@@ -34,7 +42,6 @@ public class Kata {
         }
       return winner;
     }
-
 
     public static void main(String[] args) {
         System.out.println(declareWinner(new Fighter("Lew", 10, 2),
